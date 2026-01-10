262992ba61460355718805284d56e4077210b54b
焦国栋 <jgdnew21@aliyun.com>
2026-01-10 16:34:56 +0800

    add test

---
+++ src/cim_worldlab/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["hello"]
+
+def hello() -> str:
+    """Minimal function to verify import / test / debug pipeline."""
+    return "hello cim worldlab"
---
+++ tests/test_hello.py
@@ -0,0 +1,4 @@
+from cim_worldlab import hello
+
+def test_hello():
+    assert hello() == "hello cim worldlab"
